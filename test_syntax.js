0aa159b1bf5130c651068289ca5cb9569a0c44da
Tom1419-git <thomas.mayoraz@eduvaud.ch>
2026-06-23 14:03:01 +0200

    Feat: Ajout de la page Snippets et du widget de statut en direct

---
+++ test_syntax.js
@@ -0,0 +1,5 @@
+﻿var fs = new ActiveXObject("Scripting.FileSystemObject");
+var frToEn = eval("(" + fs.OpenTextFile("E:\\Perso\\Site_Web\\media\\locales\\en.js", 1).ReadAll().replace('const frToEn = ', '') + ")");
+WScript.Echo("EN OK");
+var frToDe = eval("(" + fs.OpenTextFile("E:\\Perso\\Site_Web\\media\\locales\\de.js", 1).ReadAll().replace('const frToDe = ', '') + ")");
+WScript.Echo("DE OK");
