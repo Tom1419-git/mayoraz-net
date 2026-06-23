3a64fd3afe10ea7af24538919d203912bf12efcb
Tom1419-git <thomas.mayoraz@eduvaud.ch>
2026-06-23 14:03:23 +0200

    Fix: Nettoyage des fichiers de test générés

--- test_en.js
+++
@@ -1,9 +0,0 @@
-﻿var fs = new ActiveXObject("Scripting.FileSystemObject");
-try {
-    var enTxt = fs.OpenTextFile("E:\\Perso\\Site_Web\\media\\locales\\en.js", 1, false, -1).ReadAll().replace('const frToEn = ', '');
-    // WScript.Echo(enTxt.substring(enTxt.length - 100));
-    eval("(" + enTxt + ")");
-    WScript.Echo("EN OK");
-} catch(e) {
-    WScript.Echo("EN ERROR: " + e.message);
-}
